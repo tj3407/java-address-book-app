f31b64761d5c9de7f2648631888bd69c194608ed
TJ Sornet <33141343+tj3407@users.noreply.github.com>
2021-01-20 19:05:21 -0800

    Initial commit

---
+++ src/com/example/AddressBookApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class AddressBookApplication {
+    public static void main(String[] args) {
+        Menu.prompt_FirstName();
+        Menu.prompt_LastName();
+        Menu.prompt_Street();
+        Menu.prompt_City();
+        Menu.prompt_State();
+        Menu.prompt_Zip();
+        Menu.prompt_Telephone();
+        Menu.prompt_Email();
+    }
+}
---
+++ src/com/example/Menu.java
@@ -0,0 +1,28 @@
+package com.example;
+
+public class Menu {
+    public static void prompt_FirstName() {
+        System.out.println("First Name:");
+    }
+    public static void prompt_LastName() {
+        System.out.println("Last Name:");
+    }
+    public static void prompt_Street() {
+        System.out.println("Street:");
+    }
+    public static void prompt_City() {
+        System.out.println("City:");
+    }
+    public static void prompt_State() {
+        System.out.println("State:");
+    }
+    public static void prompt_Zip() {
+        System.out.println("Zip:");
+    }
+    public static void prompt_Telephone() {
+        System.out.println("Telephone:");
+    }
+    public static void prompt_Email() {
+        System.out.println("Email:");
+    }
+}
